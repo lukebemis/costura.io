26dc660cb1d627103d1c728075df62467dbd3da5
Lucas Ritter <lucasjritter@gmail.com>
2023-06-02 15:43:59 -0300

    tentativa

---
+++ src/Page1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page1 = () => {
+  return (
+    <div>
+      <h1>Página 1</h1>
+      <p>Conteúdo da página 1</p>
+    </div>
+  );
+};
+
+export default Page1;
