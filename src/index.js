72c7766e2df1cdef8dfe20ee055e54feec415a2e
Lucas Ritter <lucasjritter@gmail.com>
2023-06-23 16:45:34 -0300

    22

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-document.title = "Costura Virtual ";
+document.title = "costura.io";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
